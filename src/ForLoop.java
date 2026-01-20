e1491b8e9be99a4741040211bbc0316c1553eeab
Om Shinde <Shindeom942@gmail.com>
2026-01-20 19:38:10 +0530

    init commit

---
+++ src/ForLoop.java
@@ -0,0 +1,12 @@
+public class ForLoop {
+
+    public void operation() {
+        for(int i=1;i<=11;i=i+1){
+            System.out.println(i);
+        }
+    }
+    public static void main (String[] args){
+        ForLoop obj=new ForLoop();
+        obj.operation();
+    }
+}
